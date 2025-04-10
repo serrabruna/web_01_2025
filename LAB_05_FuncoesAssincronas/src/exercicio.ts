3a7275377832ec01f709c261fb1ee703f46dadcd
serrabruna <brunaibserra@hotmail.com>
2025-04-10 14:34:57 -0300

    Finalização do exercicio do LAB5- Funções Assincronas

--- LAB_05_FuncoesAssincronas/src/exercicio.ts
+++ LAB_05_FuncoesAssincronas/src/exercicio.ts
@@ -2,17 +2,45 @@ type Tarefa = {
     descricao: string;
     prioridade: number;
     concluida: boolean;
-}
+};
 
-const tarefas : Tarefa[] = [
-    {descricao: "Fazer relatório", prioridade: 2, concluida: false },
-    {descricao: "Enviar e- mail", prioridade: 3, concluida: false },
-    {descricao: "Reunião com equipe", prioridade: 1, concluida: false }
+const tarefas: Tarefa[] = [
+    { descricao: "Fazer relatório", prioridade: 2, concluida: false },
+    { descricao: "Enviar e-mail", prioridade: 3, concluida: false },
+    { descricao: "Reunião com equipe", prioridade: 1, concluida: false }
 ];
 
-function imprimirTarefa (tarefas: Tarefa[],descricao: string, indice: number, totalTarefas: number ) : void {
-    console.log (Tarefa concluída: ${descricao});
-    console.log (Progresso: ${indice + 1}/ ${totalTarefas});
+function executarTarefas(
+    tarefas: Tarefa[],
+    callback: (descricao: string, indice: number, total: number) => void,
+    intervalo: number = 1000
+    ): void {
+    tarefas.sort((a, b) => a.prioridade - b.prioridade);
+    let indice: number = 0;
+    const total: number = tarefas.length;
+
+    const id = setInterval(() => {
+        if (indice >= total) {
+            clearInterval(id);
+            return;
+        }
+
+        const tarefa = tarefas[indice];
+
+        if (tarefa.descricao === "Cancelar") {
+            clearInterval(id);
+            return;
+        }
+
+        tarefa.concluida = true;
+        callback(tarefa.descricao, indice, total);
+        indice++;
+    }, intervalo);
 }
 
-executarTarefas (tarefas, imprimirTarefa, 1500);
+function imprimirTarefa(descricao: string, indice: number, totalTarefas: number): void {
+    console.log(`Tarefa concluída: ${descricao}`);
+    console.log(`Progresso: ${indice + 1}/${totalTarefas}`);
+}
+
+executarTarefas(tarefas, imprimirTarefa, 1500);
